315b39e92b4097af95860013b085983fad8050b2
CherryYang <754449629@qq.com>
2022-11-11 00:31:39 +0800

    All Done

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -5,7 +5,7 @@
 fn main() {
     let my_option: Option<()> = None;
     if my_option.is_none() {
-        panic!();
+        println!("this is None");
     }
 
     let my_arr = &[
@@ -21,8 +21,6 @@ fn main() {
     let mut value_a = 45;
     let mut value_b = 66;
     // Let's swap these two!
-    // value_a = value_b;
-    // value_b = value_a;
     std::mem::swap(&mut value_a, &mut value_b);
     println!("value a: {}; value b: {}", value_a, value_b);
 }
